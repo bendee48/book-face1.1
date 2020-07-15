1e42f76e5c3659088fb054d34ef9ce7a34f8d560
Ben Daniels <benbanj@hotmail.com>
2020-07-15 18:24:51 +0100

    Adding sign in header

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root 'users#index'
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
