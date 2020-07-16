39f8ef5736943ca755c1cf4f49bcaaae34732e5f
Ben Daniels <benbanj@hotmail.com>
2020-07-16 15:29:23 +0100

    Adding seed data and User has_many destroy dependency

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Populate Users
+User.destroy_all
+User.create!(name: 'Ben', email: 'ben@email.com', password: 'password')
+User.create!(name: 'Emma', email: 'emma@email.com', password: 'password')
+User.create!(name: 'Ruth', email: 'ruth@email.com', password: 'password')
+User.create!(name: 'Beth', email: 'beth@email.com', password: 'password')
+User.create!(name: 'Ryan', email: 'ryan@email.com', password: 'password')
