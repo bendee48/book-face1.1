8cf04d0ea9ba9462e8d07021e823010b916118d2
Ben Daniels <benbanj@hotmail.com>
2020-07-19 13:12:02 +0100

    Moving all frienship validation to custom validator

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -6,7 +6,7 @@ class Friendship < ApplicationRecord
   belongs_to :friend, foreign_key: :friend_id, class_name: 'User'
   belongs_to :friend_of, foreign_key: :friend_of_id, class_name: 'User'
 
-  validates :friend, uniqueness: { scope: :friend_of_id, message: "Already friends." }
-  validates :friend_of, uniqueness: { scope: :friend_id, message: "Already friends." }
+  # validates :friend, uniqueness: { scope: :friend_of_id, message: "Already friends." }
+  # validates :friend_of, uniqueness: { scope: :friend_id, message: "Already friends." }
   validates_with FriendshipValidator
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   has_many :friend_friendships, foreign_key: :friend_of_id, class_name: 'Friendship', dependent: :destroy
-  has_many :friends, through: :friend_friendships, source: :friend, autosave: true
+  has_many :friends, through: :friend_friendships, source: :friend
   
   has_many :friend_of_friendships, foreign_key: :friend_id, class_name: 'Friendship', dependent: :destroy
-  has_many :friend_ofs, through: :friend_of_friendships, source: :friend_of, autosave: true
+  has_many :friend_ofs, through: :friend_of_friendships, source: :friend_of
 
   has_many :friend_requests, dependent: :destroy
   has_many :request_users, through: :friend_requests, source: :request_user
