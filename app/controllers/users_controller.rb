e14696be2b57e70d2178200d3c7194f7ef3295b5
Ben Daniels <benbanj@hotmail.com>
2020-07-21 11:51:20 +0100

    Hide friend button if already friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
+  helper_method :friends_already?
+  
   def index
     @users = User.all
   end
 
   def show
-    @user = User.includes(:friends).find(params[:id])    
+    @user = User.includes(:friends).find(params[:id])
+  end
+
+  def friends_already?(friend)
+    current_user.all_friends.include?(friend)
   end
 end
