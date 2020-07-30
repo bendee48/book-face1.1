15700337c491f9eaf317b755aa7ffcd37fb91f14
Ben Daniels <benbanj@hotmail.com>
2020-07-30 17:22:42 +0100

    Add photo upload validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,18 @@ class Post < ApplicationRecord
   validates :body, presence: true
   validates_associated :comments
   validates_associated :likes, message: "Already liked."
+  validate :accept_image
 
   def self.posts_of_user_and_friends(user)
     where(user_id: user.friend_ids << user.id).order(created_at: :desc)
+  end
+
+  def accept_image
+    return unless photo.attached?
+
+    accepted_types = ['image/jpeg', 'image/png']
+    unless accepted_types.include?(photo.content_type)
+      errors.add(:photo, 'must be a JPG or PNG.')
+    end
   end
 end
