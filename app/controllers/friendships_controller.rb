9196da20b65aac3158c715f200749b0cf3f497fa
Ben Daniels <benbanj@hotmail.com>
2020-08-06 15:41:16 +0100

    Refactoring Friendship create action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,16 +1,17 @@
 class FriendshipsController < ApplicationController
   def create
-    @user = User.find(current_user.id)
     @potential_friend = User.find(params[:user_id])
-
-    begin
-      @user.friends << @potential_friend
-    rescue
-      flash.notice = "Already friends."
+    @friendship = Friendship.new(friend_id: @potential_friend.id, friend_of_id: current_user.id)
+    
+    if @friendship.save
+      flash.notice = "You are now friends with #{@potential_friend.name}"
+      redirect_to current_user
+    else
+      flash.notice = "You are already friends."
+      redirect_to current_user
     end
 
-    @user.request_users.destroy(User.find(@potential_friend.id))
-
-    redirect_to @user
+    #delete friend request
+    current_user.request_users.destroy(User.find(@potential_friend.id))
   end
 end
