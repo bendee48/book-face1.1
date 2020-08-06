a5c1c14f82fc86c7f8a852b2fe67ae11bed089c6
Ben Daniels <benbanj@hotmail.com>
2020-08-06 17:46:46 +0100

    Adding unfriending

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -14,4 +14,15 @@ class FriendshipsController < ApplicationController
     #delete friend request
     current_user.request_users.destroy(User.find(@potential_friend.id))
   end
+
+  def destroy
+    @friendship = Friendship.find_by(friend_id: current_user.id, friend_of_id: params[:id]) ||
+                  Friendship.find_by(friend_id: params[:id], friend_of_id: current_user.id)
+
+    @friendship.destroy
+
+    flash.notice = "Friend removed."
+
+    redirect_back fallback_location: root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   end
   resources :likes, only: [:create]
   resources :comments, only: [:create, :destroy]
-  resources :friendships, only: [:create]
+  resources :friendships, only: [:create, :destroy]
   resources :friend_requests, only: [:create, :index, :destroy]
 end
