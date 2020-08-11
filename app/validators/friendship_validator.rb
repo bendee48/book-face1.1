0883f54551f2c045123d6997676ef0043611ec80
Ben Daniels <benbanj@hotmail.com>
2020-08-11 16:40:49 +0100

    Adding Friendship model tests

--- app/validators/friendship_validator.rb
+++ app/validators/friendship_validator.rb
@@ -1,6 +1,6 @@
 class FriendshipValidator < ActiveModel::Validator 
   def validate(record)
-    if (record.class.where('friend_id = ? AND friend_of_id = ?', record.friend_of, record.friend).exists? ||
+    if (record.class.where('friend_id = ? AND friend_of_id = ?', record.friend_of, record.friend).exists? &&
         record.class.where('friend_id = ? AND friend_of_id = ?', record.friend, record.friend_of).exists?)
           record.errors[:friendship] << "Friendship already exists!"
     end
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  
+  subject { create :friendship }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is invalid without a valid friend_id" do
+    subject.friend_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is invalid without a valid friend_of_id" do
+    subject.friend_of_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is invalid if friendship already exists" do
+    friend_id = subject.friend_of_id
+    friend_of_id = subject.friend_id
+    friendship = create :friendship, friend_id: friend_id, friend_of_id: friend_of_id
+    expect(friendship).to_not be_valid
+  end
 end
